5905122703d00629bc6291ece752dc10171a6b9f
Sorfian <sorfian.tkj@gmail.com>
2023-03-29 16:49:28 +0800

    design UI

---
+++ RecordPro/RecordProViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RecordProViewController.swift
+//  RecordPro
+//
+//  Created by Sorfian on 29/03/23.
+//
+
+import UIKit
+
+class RecordProViewController: UIViewController {
+    
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @IBAction func play(_ sender: UIButton) {
+    }
+    
+    @IBAction func record(_ sender: UIButton) {
+    }
+    
+    @IBAction func stop(_ sender: UIButton) {
+    }
+    
+    
+}
